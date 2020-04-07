4146a9b29e6b2092f4277d15174b027a480fc776
Bla <21099480+XiaoLongBaoXL@users.noreply.github.com>
2020-04-07 17:09:41 +0200

    Improved code

--- CoordinateRow.py
+++ CoordinateRow.py
@@ -2,6 +2,6 @@ class CoordinateRow:
     def __init__(self):
         self.coordinate_row = []
 
-    def append(self,coordinate):
+    def append(self, coordinate):
         self.coordinate_row.append(coordinate)
         return self.coordinate_row
--- Snake.py
+++ Snake.py
@@ -2,137 +2,136 @@ from ipy_lib import SnakeUserInterface  #import interface
 from Coordinate import Coordinate
 from CoordinateRow import CoordinateRow
 
-WIDTH = 32
-HEIGHT = 24
+FIELD_WIDTH = 32
+FIELD_HEIGHT = 24
 FPS = 10
 RIGHT = 'r'
 LEFT = 'l'
 UP = 'u'
 DOWN = 'd'
-
-ui = SnakeUserInterface(WIDTH,HEIGHT)   #create GUI object
-ui.set_animation_speed(FPS)
-
-apple = Coordinate(0,0)
-apple_amount = 0
-snake_direction = Coordinate(1,0)
-snake_length = 2
-direction = RIGHT
+apple_stock = CoordinateRow()
 
 '''
 Functions
 '''
-def process_event(event):
-    global snake_direction
-    global direction
-    global snake_row
-    global snake_length
-    global apple
-    global apple_amount
-    no_borders(snake_head)
-    
-    if event.name == 'arrow':
-        if event.data == RIGHT and direction != LEFT:
-            snake_direction.x = 1
-            snake_direction.y = 0
-            direction = RIGHT
-        if event.data == LEFT and direction != RIGHT:
-            snake_direction.x = -1
-            snake_direction.y = 0
-            direction = LEFT
-        if event.data == DOWN and direction != UP:
-            snake_direction.x = 0
-            snake_direction.y = 1
-            direction = DOWN
-        if event.data == UP and direction != DOWN:
-            snake_direction.x = 0
-            snake_direction.y = -1
-            direction = UP
-        
-    if event.name == 'alarm':
-        if len(snake_row) > (snake_length):
-            remove_item(snake_row[0])
-            del snake_row[0]
-        
-        for coordinate in snake_row:
-            no_borders(coordinate)
-            place_snake(coordinate)
-        
-        #snake eats apple
-        if snake_head.x == apple.x and snake_head.y == apple.y:
-            snake_length += 1
-            apple_amount = 0
+def create_field(width=FIELD_WIDTH, height=FIELD_HEIGHT, speed=FPS):
+    ui = SnakeUserInterface(width, height)   #create GUI object
+    ui.set_animation_speed(speed)
+    return ui
 
-        #snake eats himself
-        for coordinate in snake_row[0:(snake_length-2)]:
-            if snake_head.x == coordinate.x and snake_head.y == coordinate.y:
-                game_over()
-                
-        snake_head.x += snake_direction.x
-        snake_head.y += snake_direction.y
-        place_apple(snake_row)
-        snake_row += CoordinateRow().append(Coordinate(snake_head.x, snake_head.y))
-          
-def place_snake(coordinate):
-    ui.place(coordinate.x,coordinate.y,ui.SNAKE)
-    coordinate = Coordinate(coordinate.x,coordinate.y)
-    ui.show()
-    return coordinate
-        
-def remove_item(coordinate):
-    ui.place(coordinate.x,coordinate.y,ui.EMPTY)
-    ui.show()
+def initialize_snake():
+    snake_tail = Coordinate(0,0)
+    snake_head = Coordinate(1,0)
+    snake_row = CoordinateRow().append(Coordinate(snake_tail.x, snake_tail.y))
+    snake_row += CoordinateRow().append(Coordinate(snake_head.x, snake_head.y))
+    snake_direction = Coordinate(1,0)
+    snake_length = len(snake_row)
+    return snake_row, snake_direction, snake_length
 
-def no_borders(coordinate):
-    if coordinate.x == WIDTH:
+def place_snake(field, coordinate, alive=True):
+    if alive:
+        field.place(coordinate.x, coordinate.y, game_field.SNAKE)
+    if not alive:
+        field.place_transparent(coordinate.x, coordinate.y, game_field.SNAKE)
+    field.show()
+
+def create_apple(field):
+    apple_coordinate = Coordinate()
+    apple_coordinate.x = field.random(field.snake_interface.width)
+    apple_coordinate.y = field.random(field.snake_interface.height)
+    return apple_coordinate
+
+def place_apple(field, coordinate_row, apple_coordinate_row):
+    apple = create_apple(field)
+    for coordinate in coordinate_row:
+        if apple.x == coordinate.x and apple.y == coordinate.y:
+            apple = create_apple(field)
+    field.place(apple.x, apple.y, field.FOOD)
+    field.show()
+    apple_coordinate_row.append(apple)
+
+def remove_item(field, coordinate):
+    field.place(coordinate.x, coordinate.y, field.EMPTY)
+    field.show()
+
+def border_action(field, coordinate):
+    if coordinate.x == field.snake_interface.width:
         coordinate.x = 0
     if coordinate.x < 0:
-        coordinate.x = WIDTH-1
-    if coordinate.y == HEIGHT:
+        coordinate.x = field.snake_interface.width-1
+    if coordinate.y == field.snake_interface.height:
         coordinate.y = 0
     if coordinate.y < 0:
-        coordinate.y = HEIGHT-1
+        coordinate.y = field.snake_interface.height-1
     return coordinate
 
-def place_apple(row):
-    global apple_amount
-    global apple
-    if apple_amount == 1:
-        return
-    else:
-        apple.x = ui.random(WIDTH)
-        apple.y = ui.random(HEIGHT)
-        for coordinate in row:
-            if apple.x == coordinate.x and apple.y == coordinate.y:
-                place_apple(row)
-        ui.place(apple.x,apple.y,ui.FOOD)
-        ui.show()
-        apple_amount = 1
-        return
+def game_over(field, event):
+    time_left = 10
+    field.print_("Game over!\nThis window will close in.. ")
+    for i in range(time_left, 0, -1):
+        field.print_(str(i))
+        field.print_(".. ")
+        field.wait(1000)
+    field.close()
 
-def game_over():
-    ui.print_("Game over\nThis window will close in 5..")
-    ui.wait(1000)
-    ui.print_("4..")
-    ui.wait(1000)
-    ui.print_("3..")
-    ui.wait(1000)
-    ui.print_("2..")
-    ui.wait(1000)
-    ui.print_("1..")
-    ui.wait(1000)
-    ui.close()
+def game_process(field, event, coordinate_row):
+    global apple_stock
+    snake_direction = coordinate_row[1]
+    snake_length = coordinate_row[2]
+    new_head_coordinate = Coordinate()
+
+    if event.name == 'arrow':
+        if event.data == RIGHT and snake_direction.x != -1:
+            snake_direction.x = 1
+            snake_direction.y = 0
+        if event.data == LEFT and snake_direction.x != 1:
+            snake_direction.x = -1
+            snake_direction.y = 0
+        if event.data == DOWN and snake_direction.y != -1:
+            snake_direction.x = 0
+            snake_direction.y = 1
+        if event.data == UP and snake_direction.y != 1:
+            snake_direction.x = 0
+            snake_direction.y = -1
+    
+    if event.name == 'alarm':
+        if len(coordinate_row[0]) > snake_length:
+            remove_item(field, coordinate_row[0][0])
+            del coordinate_row[0][0]
+        
+        for coordinate in coordinate_row[0]:
+            border_action(field, coordinate)
+            place_snake(field, coordinate, True)
+
+        if len(apple_stock.coordinate_row) == 0:
+            place_apple(field, coordinate_row[0], apple_stock)
+        
+        #snake eats apple
+        if coordinate_row[0][-1].x == apple_stock.coordinate_row[0].x and coordinate_row[0][-1].y == apple_stock.coordinate_row[0].y:
+            snake_length += 1
+            del apple_stock.coordinate_row[0]
+            new_tail_coordinate = Coordinate(coordinate_row[0][0].x, coordinate_row[0][0].y)
+            coordinate_row[0].insert(0, new_tail_coordinate)
+        
+        #snake eats himself
+        for coordinate in coordinate_row[0][0:-2]:
+            if coordinate_row[0][-1].x == coordinate.x and coordinate_row[0][-1].y == coordinate.y:
+                for coordinate in coordinate_row[0]:
+                    place_snake(field, coordinate, False)
+                game_over(field, event)
+
+        new_head_coordinate.x = coordinate_row[0][-1].x + snake_direction.x
+        new_head_coordinate.y = coordinate_row[0][-1].y + snake_direction.y
+        new_head_coordinate = Coordinate(new_head_coordinate.x, new_head_coordinate.y)
+        coordinate_row[0].append(new_head_coordinate)
 
 '''
 Program
 '''
-#starting positions
-snake_tail = Coordinate(0,0)
-snake_head = Coordinate(1,0)
+game_field = create_field()
+snake = initialize_snake()
 
-snake_row = CoordinateRow().append(Coordinate(snake_tail.x,snake_tail.y))
-snake_row += CoordinateRow().append(Coordinate(snake_head.x,snake_head.y))
-
-while True: #infinite loop
-    event = ui.get_event()
-    process_event(event)
+game_field.print_("Welcome!\nUse the arrow keys on your keyboard to play.")
+while True:
+    event = game_field.get_event()
+    game_process(game_field, event, snake)
